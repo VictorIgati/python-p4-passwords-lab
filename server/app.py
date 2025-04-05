cef810cf83b7d24d896c826aabcb4b595a6f28d0
VictorIgati <victor.igati@student.moringaschool.com>
2025-04-05 10:02:40 +0300

    Implement user session management with login, logout, and check session endpoints

--- server/app.py
+++ server/app.py
@@ -7,16 +7,12 @@ from config import app, db, api
 from models import User
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
         json = request.get_json()
         user = User(
@@ -25,19 +21,35 @@ class Signup(Resource):
         user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
+        session['user_id'] = user.id
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict()
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter(User.username == json['username']).first()
+        if user.authenticate(json['password']):
+            session['user_id'] = user.id
+            return user.to_dict()
+        return {'error': 'Invalid credentials'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
